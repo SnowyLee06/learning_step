fc617cd21a17a93954cb7da72664e881a784cb9d
SnowyLee06 <2451223884@qq.com>
2026-02-04 17:38:48 +0800

    Update ColumnMeta.java file to define the ColumnMeta class with table and column attributes.

---
+++ backend/src/main/java/com/snowylee/mapper/model/ColumnMeta.java
@@ -0,0 +1,10 @@
+package com.snowylee.mapper.model;
+
+public class ColumnMeta {
+    public String tableName;
+    public String columnName;
+    public String dataType;
+    public Integer length;
+    public boolean nullable;
+    public String description;
+}
