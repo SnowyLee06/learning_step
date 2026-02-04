564d17b9d0980984748cd075b1534cace5061829
SnowyLee06 <2451223884@qq.com>
2026-02-04 17:26:52 +0800

    Update MappingRecord.java with the new model structure

---
+++ backend/src/main/java/com/snowylee/mapper/model/MappingRecord.java
@@ -0,0 +1,9 @@
+package com.snowylee.mapper.model;
+
+public class MappingRecord {
+    public String sourceExpr;
+    public String targetField;
+    public String comment;
+    public String tableName;
+    public String columnName;
+}
