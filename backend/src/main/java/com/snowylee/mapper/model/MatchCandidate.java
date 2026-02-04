39af1c1f7cdbd4948993ee8486fad6ba0a8f6335
SnowyLee06 <2451223884@qq.com>
2026-02-04 17:37:29 +0800

    Update MatchCandidate.java

---
+++ backend/src/main/java/com/snowylee/mapper/model/MatchCandidate.java
@@ -0,0 +1,8 @@
+package com.snowylee.mapper.model;
+
+public class MatchCandidate {
+    public String tableName;
+    public String columnName;
+    public double score;
+    public String reason;
+}
