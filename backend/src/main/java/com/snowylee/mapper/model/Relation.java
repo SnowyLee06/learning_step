5a61f6690592e56478c30870a348f03b46cfac39
SnowyLee06 <2451223884@qq.com>
2026-02-04 17:33:46 +0800

    Update Relation.java with new model properties

---
+++ backend/src/main/java/com/snowylee/mapper/model/Relation.java
@@ -0,0 +1,9 @@
+package com.snowylee.mapper.model;
+
+public class Relation {
+    public String leftTable;
+    public String leftColumn;
+    public String rightTable;
+    public String rightColumn;
+    public String relationType;
+}
