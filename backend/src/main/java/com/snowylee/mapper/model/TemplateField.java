f2077902017ec85b28f62b9a2c3a038b7359f584
SnowyLee06 <2451223884@qq.com>
2026-02-04 17:37:42 +0800

    Update TemplateField.java with new class definition on 2026-02-04 09:25:45 UTC.

---
+++ backend/src/main/java/com/snowylee/mapper/model/TemplateField.java
@@ -0,0 +1,9 @@
+package com.snowylee.mapper.model;
+
+public class TemplateField {
+    public String code;
+    public String name;
+    public String description;
+    public String dataType;
+    public boolean nullable;
+}
