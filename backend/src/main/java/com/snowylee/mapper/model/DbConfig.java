2ef876fa1b4fa72bdf7d60456639d615da156835
SnowyLee06 <2451223884@qq.com>
2026-02-04 17:36:48 +0800

    Updated DbConfig.java with database configuration fields

---
+++ backend/src/main/java/com/snowylee/mapper/model/DbConfig.java
@@ -0,0 +1,9 @@
+package com.snowylee.mapper.model;
+
+public class DbConfig {
+    public String host;
+    public int port;
+    public String database;
+    public String username;
+    public String password;
+}
